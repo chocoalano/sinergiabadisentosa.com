1fb276200074debb75ed61f73e70ecde46bd7dc0
chocoalano <alangentina95@gmail.com>
2024-04-05 16:05:44 +0700

    sdgdfgsdfgsdfg

--- app/Http/Controllers/Page/PrivacyPolicy.php
+++ app/Http/Controllers/Page/PrivacyPolicy.php
@@ -10,12 +10,11 @@ class PrivacyPolicy extends Controller
     {
         $pp = \App\Models\Services\PrivacyPolicy::where('publish', 1)->orderBy('created_at','desc')->first();
         $content=[
-            "page-title"=>$this->contentjson()["page-product"]["title-page"],
-            "page-keywords"=>$this->contentjson()["page-product"]["keywords-seo"],
-            "page-description"=>$this->contentjson()["page-product"]["description-seo"],
+            "page-title"=>"Privacy & policy",
+            "page-keywords"=>"",
+            "page-description"=>"",
             "header"=>$this->header(),
             "footer"=>$this->footer(),
-            "jumbotron"=>$this->contentjson()["page-product"]["jumbotron"]
         ];
         return view("privacyPolicy", compact("content", "pp"));
     }
